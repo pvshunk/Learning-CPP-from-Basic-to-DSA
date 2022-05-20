6d5bd8fcbfe37070e3b9ae1fb1db71b7d6c4dfb1
pvshunk <66204496+pvshunk@users.noreply.github.com>
2022-05-20 17:03:59 +0530

    Add files via upload

---
+++ Decimal to Binary.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+	//Decimal to Binary
+	
+	int n;
+	cin>>n;
+	
+	int ans = 0;
+	
+	int i = 0;
+	
+	while(n!=0){
+
+	    int bit = n&1;//This formula provides the binary value of any no by comparing with 1 under & operator
+
+		ans = (bit*pow(10,i))+ans;//To find reverse of ans
+		//ans = (10*ans)+bit;To find ans without reverse
+		
+		n = n >> 1 ;
+		i++;
+
+	}
+	cout<<ans;
+
+return 0;
+}
+
