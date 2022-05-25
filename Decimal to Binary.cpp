12626c500a7d6b854c9379cd4a9e3e735bec5d93
pvshunk <66204496+pvshunk@users.noreply.github.com>
2022-05-26 01:02:26 +0530

    Update Decimal to Binary.cpp

--- Decimal to Binary.cpp
+++ Decimal to Binary.cpp
@@ -14,7 +14,7 @@ int main(){
 	
 	while(n!=0){
 
-	    int bit = n&1;//This formula provides the binary value of any no by comparing with 1 under & operator
+	    int bit = n&1;//This formula provides the exact binary of decimal.The binary value of any no by comparing with 1 under & operator
 
 		ans = (bit*pow(10,i))+ans;//To find reverse of ans
 		//ans = (10*ans)+bit;To find ans without reverse
